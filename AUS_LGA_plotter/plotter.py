24e4afe14cedec15173e77ceb615fe80a773861a
bananabite <72071891+bananabite@users.noreply.github.com>
2024-03-19 18:06:34 +1100

    updated plotter.py

--- AUS_LGA_plotter/plotter.py
+++ AUS_LGA_plotter/plotter.py
@@ -1,4 +1,4 @@
 # https://www.kaggle.com/code/shtrausslearning/geospatial-data-visualisation-australia
-
+# https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files
 class plot():
     
